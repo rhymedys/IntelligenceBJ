a00d3bb5d4c8b53e01ca68eac818b39a158aedd5
rhymedys <rhymelxy@126.com>
2016-09-11 19:51:34 +0800

    init

--- app/src/main/java/com/example/rhymedys/intelligencebj/SplashActivity.java
+++ app/src/main/java/com/example/rhymedys/intelligencebj/SplashActivity.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     private Context context;
 
