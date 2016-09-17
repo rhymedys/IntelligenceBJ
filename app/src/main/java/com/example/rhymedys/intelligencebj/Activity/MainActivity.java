bab5d4eebe625947fc32019032f1b078199143bd
rhymedys <rhymedys@gmail.com>
2016-09-17 22:46:27 +0800

    init slidingMenu

--- app/src/main/java/com/example/rhymedys/intelligencebj/Activity/MainActivity.java
+++ app/src/main/java/com/example/rhymedys/intelligencebj/Activity/MainActivity.java
@@ -5,15 +5,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.rhymedys.intelligencebj.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
+    private SlidingMenu slidingMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.context=this;
+        this.context = this;
+
+        initUI();
+
+
+    }
+
+    private void initUI() {
+        newSlidingMenu();
+    }
+
+    /**
+     * 初始化一个SlidingMenu
+     */
+    private void newSlidingMenu() {
+        slidingMenu = new SlidingMenu(context);
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        // 设置触摸屏幕的模式
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setShadowWidthRes(R.dimen.shadow_width);
+//        slidingMenu.setShadowDrawable(R.drawable.shadow);
+
+        // 设置滑动菜单视图的宽度
+        slidingMenu.setBehindOffset(200);
+//        slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        // 设置渐入渐出效果的值
+        slidingMenu.setFadeDegree(0.35f);
+        /**
+         * SLIDING_WINDOW will include the Title/ActionBar in the content
+         * section of the SlidingMenu, while SLIDING_CONTENT does not.
+         */
+        slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        //为侧滑菜单设置布局
+        slidingMenu.setMenu(R.layout.left_menu);
     }
 }
